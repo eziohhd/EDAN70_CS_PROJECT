c9ca8b055dbd460eb5251c30108a9ffc1a0ca946
xdlixx <90400468+xdlixx@users.noreply.github.com>
2021-11-28 23:38:08 +0100

    fix some bugs

--- Arduino/SerialEvent/SerialEvent.ino
+++ Arduino/SerialEvent/SerialEvent.ino
@@ -42,6 +42,8 @@ int distance;
 boolean scanStart  = false;     // decide when the sacn should start
 boolean calibrationStart  = false;     // decide when the sacn should start
 
+int mappedTiltCurrentPosition=0;
+int mappedTiltCurrentPosition=0;
 int servoMoving1 = 0;            //whether or not the servo is moving, 0 = not moving, 1 = moving
 int panGoalPositon = PAN_MIN;   // Goal Position, where we want the servo to go
 int panCurrentPosition;         //Actual position of the servo
@@ -81,48 +83,51 @@ void setup()
   
   SetPosition(2,TILT_MIN);  //set the position of servo # 2 to  its defualt position. If the servo is not present, nothing will happen  TILT_MAX
   delay(33);                    //delay before next DYNAMIXEL command
-
+  
+  
+  I2c.begin();     // Opens & joins the i2c bus as master
+  delay(100);      // Waits to make sure everything is powered up before sending or receiving data  
+  I2c.timeOut(50); // Sets a timeout to ensure no locking up of sketch if I2C communication fails
   //program is ready, turn on the LED
   pinMode(0, OUTPUT);   //set pin mode 
   digitalWrite(0,HIGH); //turn LED on
   Serial.println("Ready");
 }
 
-void setup_scan()
-{
-  Serial.begin(115200); //start serial port communication 115200
-  delay(500);           //delay to let DYNAMIXEL services start  
-  
-  ax12SetRegister2(1,AX_GOAL_SPEED_L,DEFAULT_SPEED1);    //set the speed on the pan servo
-  delay(33);                                             //delay before next DYNAMIXEL command
-  ax12SetRegister2(2,AX_GOAL_SPEED_L,DEFAULT_SPEED2);    //set the speed on the tilt servo
-  delay(33);                                             //delay before next DYNAMIXEL command
-  
-  SetPosition(1,PAN_MIN);     //set the position of servo # 1 to its starting/min position
-  delay(33);                   //delay before next DYNAMIXEL command
-  
-  SetPosition(2,TILT_MIN); //set the position of servo # 2 to  its defualt position. If the servo is not present, nothing will happen
-  delay(33);                    //delay before next DYNAMIXEL command
-
-  I2c.begin();     // Opens & joins the i2c bus as master
-  delay(100);      // Waits to make sure everything is powered up before sending or receiving data  
-  I2c.timeOut(50); // Sets a timeout to ensure no locking up of sketch if I2C communication fails
-  
-  //program is ready, turn on the LED
-  pinMode(0, OUTPUT);   //set pin mode 
-  digitalWrite(0,HIGH); //turn LED on
-}
+//void setup_scan()
+//{
+//  Serial.begin(115200); //start serial port communication 115200
+//  delay(500);           //delay to let DYNAMIXEL services start  
+//  
+//  ax12SetRegister2(1,AX_GOAL_SPEED_L,DEFAULT_SPEED1);    //set the speed on the pan servo
+//  delay(33);                                             //delay before next DYNAMIXEL command
+//  ax12SetRegister2(2,AX_GOAL_SPEED_L,DEFAULT_SPEED2);    //set the speed on the tilt servo
+//  delay(33);                                             //delay before next DYNAMIXEL command
+//  
+//  SetPosition(1,PAN_MIN);     //set the position of servo # 1 to its starting/min position
+//  delay(33);                   //delay before next DYNAMIXEL command
+//  
+//  SetPosition(2,TILT_MIN); //set the position of servo # 2 to  its defualt position. If the servo is not present, nothing will happen
+//  delay(33);                    //delay before next DYNAMIXEL command
+//
+//  I2c.begin();     // Opens & joins the i2c bus as master
+//  delay(100);      // Waits to make sure everything is powered up before sending or receiving data  
+//  I2c.timeOut(50); // Sets a timeout to ensure no locking up of sketch if I2C communication fails
+//  
+//  //program is ready, turn on the LED
+//  pinMode(0, OUTPUT);   //set pin mode 
+//  digitalWrite(0,HIGH); //turn LED on
+//}
 
 
 void loop() {
-    
   //*detect start scan signal
   if (inputString == "StartScanning")
   {
-      setup_scan();
+//      setup_scan();
       scanStart = true;
-      Serial.println("Start scanning");
-//      inputString = "";
+    // Serial.println("Start scanning");
+      inputString = "";
           
   }
   
@@ -130,16 +135,94 @@ void loop() {
   {
       scanStart = false;
       calibrationStart = true;
-     // Serial.println("Start calibration");       
+     // Serial.println("Start calibration");  
+      inputString = "";     
   }
   
   
   
-  while(scanStart)
+  if (scanStart)
   {
+//  scan();
+  if(mappedTiltCurrentPosition >= TILT_MAX)
+  {
+     Serial.println(tiltCurrentPosition);
+     Serial.println(mappedTiltCurrentPosition);
+    Serial.println("Scan done");
+    scanActive = false;
+  }
+  else
+  {
+    scanActive = true;
+  }
+  //Serial.println(scanActive);
   
-  scan();
-  
+  if(scanActive)
+  {
+    //move the pan server
+    if(millis() - previousMillisMoving > intervalMoving) 
+    {
+      // save the last time the servo was polled for moving status
+      previousMillisMoving = millis();     
+      servoMoving1 =   ax12GetRegister(1, AX_MOVING, 1);
+    }
+    //compare the currrent number if milliseconds with the last time the distance/position data was reported
+    if(millis() - previousMillisReport > intervalReport) 
+    {
+      panCurrentPosition =   ax12GetRegister(1, AX_PRESENT_POSITION_L, 2);  //get current position from servo
+      tiltCurrentPosition =   ax12GetRegister(2, AX_PRESENT_POSITION_L, 2);  //get current position from servo
+     
+      //Serial.println(panCurrentPosition);
+      //Serial.println(tiltCurrentPosition);
+      //check that the reported position is within the range (incase of bad return or no return data (-1) )
+      if(panCurrentPosition >= PAN_MIN && panCurrentPosition <= PAN_MAX) //
+      {
+        distance = llGetDistance();   
+        //Serial.println(panCurrentPosition);
+       // Serial.println(tiltCurrentPosition);
+        //get distance from LIDAR lite
+        //distance = llGetDistanceAverage(3);                                 //get distance from LIDAR lite, averaging version
+        // save the last distance/position was reported
+        previousMillisReport = millis();        
+        //map the current position to a 0-4095 / 0-360 scale. This helps us work with AX servos with a limited 
+        mappedPanCurrentPosition = map(panCurrentPosition, PAN_MIN, PAN_MAX, MAPPED_PAN_MIN, MAPPED_PAN_MAX);
+        mappedTiltCurrentPosition = map(tiltCurrentPosition, TILT_MIN, TILT_MAX, MAPPED_TILT_MIN,MAPPED_TILT_MAX);        
+             
+        Serial.println(highByte(mappedPanCurrentPosition));//position high byte
+        Serial.println(lowByte(mappedPanCurrentPosition));//position low byte
+        Serial.println(highByte(mappedTiltCurrentPosition));//position high byte
+        Serial.println(lowByte(mappedTiltCurrentPosition));//position low byte
+        Serial.println(distance);//position low byte
+
+      }
+    }
+    
+     if(servoMoving1 == 0)
+      {
+        servoMoving2 =   ax12GetRegister(2, AX_MOVING, 1);
+        tiltCurrentPosition =   ax12GetRegister(2, AX_PRESENT_POSITION_L, 2);
+        
+        if(tiltCurrentPosition >= TILT_MIN && tiltCurrentPosition <= TILT_MAX)
+        {
+        tiltGoalPositon = tiltCurrentPosition + 20;
+        if(panGoalPositon == PAN_MIN)
+        {
+          panGoalPositon = PAN_MAX;
+          
+        }
+        else
+        {
+          panGoalPositon = PAN_MIN;
+        }
+        
+        delay(5);
+        SetPosition(1,panGoalPositon); //set the position of servo # 1 to '0'
+        SetPosition(2,tiltGoalPositon); 
+        servoMoving1 = 1;
+      }
+
+      }
+  }
   }
   
   if(calibrationStart)
@@ -174,90 +257,90 @@ void loop() {
 }
 
 //*scan function
-void scan()
-{
- 
-if(tiltCurrentPosition == TILT_MAX)
-  {
-    scanActive = false;
-  }
-  else
-  {
-    scanActive = true;
-  }
-  Serial.println(scanActive);
-  
-  if(scanActive == true)
-  {
-    //move the pan server
-    if(millis() - previousMillisMoving > intervalMoving) 
-    {
-      // save the last time the servo was polled for moving status
-      previousMillisMoving = millis();     
-      servoMoving1 =   ax12GetRegister(1, AX_MOVING, 1);
-    }
-    
-    //compare the currrent number if milliseconds with the last time the distance/position data was reported
-    if(millis() - previousMillisReport > intervalReport) 
-    {
-      panCurrentPosition =   ax12GetRegister(1, AX_PRESENT_POSITION_L, 2);  //get current position from servo
-      tiltCurrentPosition =   ax12GetRegister(2, AX_PRESENT_POSITION_L, 2);  //get current position from servo
-     
-      Serial.println(panCurrentPosition);
-      Serial.println(tiltCurrentPosition);
-      //check that the reported position is within the range (incase of bad return or no return data (-1) )
-      if(panCurrentPosition >= PAN_MIN && panCurrentPosition <= PAN_MAX) //
-      {
-        distance = llGetDistance();   
-        Serial.println(panCurrentPosition);
-        Serial.println(tiltCurrentPosition);
-        //get distance from LIDAR lite
-        //distance = llGetDistanceAverage(3);                                 //get distance from LIDAR lite, averaging version
-    
-        // save the last distance/position was reported
-        previousMillisReport = millis();   
-       
-        //map the current position to a 0-4095 / 0-360 scale. This helps us work with AX servos with a limited 
-        int mappedPanCurrentPosition = map(panCurrentPosition, PAN_MIN, PAN_MAX, MAPPED_PAN_MIN, MAPPED_PAN_MAX);
-        int mappedTiltCurrentPosition = map(tiltCurrentPosition, TILT_MIN, TILT_MAX, MAPPED_TILT_MIN,MAPPED_TILT_MAX);        
-       
-        
-        Serial.println(highByte(mappedPanCurrentPosition));//position high byte
-        Serial.println(lowByte(mappedPanCurrentPosition));//position low byte
-        Serial.println(highByte(mappedTiltCurrentPosition));//position high byte
-        Serial.println(lowByte(mappedTiltCurrentPosition));//position low byte
-         Serial.println(distance);//position low byte
-
-      }
-    }
-    
-     if(servoMoving1 == 0)
-      {
-        servoMoving2 =   ax12GetRegister(2, AX_MOVING, 1);
-        tiltCurrentPosition =   ax12GetRegister(2, AX_PRESENT_POSITION_L, 2);
-        
-        if(tiltCurrentPosition >= TILT_MIN && tiltCurrentPosition <= TILT_MAX)
-        {
-        tiltGoalPositon = tiltCurrentPosition + 20;
-        if(panGoalPositon == PAN_MIN)
-        {
-          panGoalPositon = PAN_MAX;
-          
-        }
-        else
-        {
-          panGoalPositon = PAN_MIN;
-        }
-        
-        delay(5);
-        SetPosition(1,panGoalPositon); //set the position of servo # 1 to '0'
-        SetPosition(2,tiltGoalPositon); 
-        servoMoving1 = 1;
-      }
-
-      }
-  }
-}
+//void scan()
+//{
+// 
+//if(tiltCurrentPosition == TILT_MAX)
+//  {
+//    scanActive = false;
+//  }
+//  else
+//  {
+//    scanActive = true;
+//  }
+//  //Serial.println(scanActive);
+//  
+//  if(scanActive)
+//  {
+//    //move the pan server
+//    if(millis() - previousMillisMoving > intervalMoving) 
+//    {
+//      // save the last time the servo was polled for moving status
+//      previousMillisMoving = millis();     
+//      servoMoving1 =   ax12GetRegister(1, AX_MOVING, 1);
+//    }
+//    
+//    //compare the currrent number if milliseconds with the last time the distance/position data was reported
+//    if(millis() - previousMillisReport > intervalReport) 
+//    {
+//      panCurrentPosition =   ax12GetRegister(1, AX_PRESENT_POSITION_L, 2);  //get current position from servo
+//      tiltCurrentPosition =   ax12GetRegister(2, AX_PRESENT_POSITION_L, 2);  //get current position from servo
+//     
+//      //Serial.println(panCurrentPosition);
+//      //Serial.println(tiltCurrentPosition);
+//      //check that the reported position is within the range (incase of bad return or no return data (-1) )
+//      if(panCurrentPosition >= PAN_MIN && panCurrentPosition <= PAN_MAX) //
+//      {
+//        distance = llGetDistance();   
+//        //Serial.println(panCurrentPosition);
+//       // Serial.println(tiltCurrentPosition);
+//        //get distance from LIDAR lite
+//        //distance = llGetDistanceAverage(3);                                 //get distance from LIDAR lite, averaging version
+//    
+//        // save the last distance/position was reported
+//        previousMillisReport = millis();   
+//       
+//        //map the current position to a 0-4095 / 0-360 scale. This helps us work with AX servos with a limited 
+//        int mappedPanCurrentPosition = map(panCurrentPosition, PAN_MIN, PAN_MAX, MAPPED_PAN_MIN, MAPPED_PAN_MAX);
+//        int mappedTiltCurrentPosition = map(tiltCurrentPosition, TILT_MIN, TILT_MAX, MAPPED_TILT_MIN,MAPPED_TILT_MAX);        
+//       
+//        
+//        Serial.println(highByte(mappedPanCurrentPosition));//position high byte
+//        Serial.println(lowByte(mappedPanCurrentPosition));//position low byte
+//        Serial.println(highByte(mappedTiltCurrentPosition));//position high byte
+//        Serial.println(lowByte(mappedTiltCurrentPosition));//position low byte
+//        Serial.println(distance);//position low byte
+//
+//      }
+//    }
+//    
+//     if(servoMoving1 == 0)
+//      {
+//        servoMoving2 =   ax12GetRegister(2, AX_MOVING, 1);
+//        tiltCurrentPosition =   ax12GetRegister(2, AX_PRESENT_POSITION_L, 2);
+//        
+//        if(tiltCurrentPosition >= TILT_MIN && tiltCurrentPosition <= TILT_MAX)
+//        {
+//        tiltGoalPositon = tiltCurrentPosition + 20;
+//        if(panGoalPositon == PAN_MIN)
+//        {
+//          panGoalPositon = PAN_MAX;
+//          
+//        }
+//        else
+//        {
+//          panGoalPositon = PAN_MIN;
+//        }
+//        
+//        delay(5);
+//        SetPosition(1,panGoalPositon); //set the position of servo # 1 to '0'
+//        SetPosition(2,tiltGoalPositon); 
+//        servoMoving1 = 1;
+//      }
+//
+//      }
+//  }
+//}
 
 
 
@@ -311,6 +394,7 @@ void llWriteAndWait(char myAddress, char myValue){
   
   uint8_t nackack = 100; // Setup variable to hold ACK/NACK resopnses     
   while (nackack != 0){ // While NACK keep going (i.e. continue polling until sucess message (ACK) is received )
+
     nackack = I2c.write(LIDARLite_ADDRESS,myAddress, myValue); // Write to LIDAR-Lite Address with Value
     delay(2); // Wait 2 ms to prevent overpolling
     
@@ -334,13 +418,11 @@ byte llReadAndWait(char myAddress, int numOfBytes, byte arrayToSave[2]){
 
 
 int llGetDistance(){
-
   llWriteAndWait(0x00,0x04); // Write 0x04 to register 0x00 to start getting distance readings
   byte myArray[2]; // array to store bytes from read function
 
   llReadAndWait(0x8f,2,myArray); // Read 2 bytes from 0x8f
   int distance = (myArray[0] << 8) + myArray[1];  // Shift high byte [0] 8 to the left and add low byte [1] to create 16-bit int
-
   return(distance);
 }
 
