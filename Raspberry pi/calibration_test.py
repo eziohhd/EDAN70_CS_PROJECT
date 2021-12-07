5d813d839f8616548b34102c7248a0beb2a82dd6
Haidi Hu <50817489+eziohhd@users.noreply.github.com>
2021-12-07 21:52:58 +0100

    delete useless stuff

--- Raspberry pi/calibration_test.py
+++ Raspberry pi/calibration_test.py
@@ -2,7 +2,7 @@
 """
 Created on Thu Nov 11 18:53:37 2021
 
-@author: 42447
+@author: Haidi,Xingda,Sijia,Xi
 """
 import datetime
 import inquirer
@@ -115,7 +115,7 @@ if __name__ == '__main__':
                 questions = [
                 inquirer.List('next',
                     message="What do you want to do next?",
-                    choices=['Scanning', 'Calibration','beetle_test'],),
+                    choices=['Scanning', 'Calibration','Beetle_redirection'],),
                       ]
                 answers = inquirer.prompt(questions)
                 if answers['next'] == "Scanning":
@@ -132,12 +132,10 @@ if __name__ == '__main__':
                         string_cal_temp = 's'+"StartCalibration"+'\n'
                         string_cal = bytes(string_cal_temp,'utf-8')
                         ser.write(bytes(string_cal))
-                        # Get the data from Lidar and process them to get the polar points and xyz points
-                        #file_index,file = GetPointsForCalibration(T,points) #points for calibration
-                        test_index,test = GetPointsForVerification(T,points)#points for verification 
                         mode = 2
                     else:
-                        
+                        M_matrix = pd.read_csv(r'Calibration_matrix.csv',header=None)
+                        M = M_matrix.to_numpy()
                         string_cal_temp = 's'+"StartCalibration"+'\n'
                         string_cal = bytes(string_cal_temp,'utf-8')
                         ser.write(bytes(string_cal))
@@ -170,7 +168,6 @@ if __name__ == '__main__':
         kernel =  np.ones((5,5),np.uint8)
         green = cv2.morphologyEx(green, cv2.MORPH_CLOSE, kernel)
         cnts,_ = cv2.findContours(green,cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)  
-    #     for c in cnts:
         if len(cnts) != 0:
             # find the biggest countour (c) by the area
             c = max(cnts, key = cv2.contourArea)   
@@ -197,9 +194,6 @@ if __name__ == '__main__':
                     x = points[int(random_index),0]
                     y = points[int(random_index),1]
                     z = points[int(random_index),2]
-#                     x = points[int(file_index[cnt]),0]
-#                     y = points[int(file_index[cnt]),1]
-#                     z = points[int(file_index[cnt]),2
                     print(x,y,z)
                     A = Matrix(x,y,z,u,v,A)
                     cnt = cnt + 1
@@ -210,9 +204,9 @@ if __name__ == '__main__':
         # when ServoReady = 0                 
         if ServoReady == 1:
             if cnt < number_of_samples :
+                #get the random points on the roof for calibration
                  random_index = random.randrange(round(rawM.size/5*0.95),round(rawM.size/5*0.96)) 
-                 pan,tilt = GetPanTilt(random_index,rawM)  
-                 #pan,tilt = GetPanTilt(file_index[cnt],rawM)  
+                 pan,tilt = GetPanTilt(random_index,rawM)    
                  MotorControl(pan,tilt)      
                  ServoReady = 0
             else :               
@@ -223,6 +217,7 @@ if __name__ == '__main__':
                 GPIO.output(tll, GPIO.HIGH)
                 M = sol_svd(A)
                 print(M)
+                np.savetxt('Calibration_matrix.csv', np.array(M), fmt="%s",delimiter=',')
                 servoReady = 1
                 cnt = 0               
                 mode = 3
@@ -238,7 +233,6 @@ if __name__ == '__main__':
         kernel =  np.ones((5,5),np.uint8)
         green = cv2.morphologyEx(green, cv2.MORPH_CLOSE, kernel)
         cnts,_ = cv2.findContours(green,cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)  
-    #     for c in cnts:
         if len(cnts) != 0:
             # find the biggest countour (c) by the area
             c = max(cnts, key = cv2.contourArea)   
@@ -264,7 +258,7 @@ if __name__ == '__main__':
                     print(u,v)
                     print(p3)
                     # Add x, y, z
-                    x = points[int(random_index),0] #test_index[cnt]
+                    x = points[int(random_index),0] 
                     y = points[int(random_index),1]
                     z = points[int(random_index),2]
                     print(x,y,z)
@@ -282,74 +276,44 @@ if __name__ == '__main__':
         # when ServoReady = 0                 
         if ServoReady == 1:
             if cnt == 0 :
-                # Add x, y, z
-#                 x = file[cnt * 3]
-#                 y = file[cnt * 3 + 1]
-#                 z = file[cnt * 3 + 2]
-#                 pan, tilt = cart2sph(x, y, z)
-                random_index = random.randrange(round(rawM.size/5*0.95),round(rawM.size/5*0.96))
+                #get the random points on the roof for calibration verification
+                random_index = random.randrange(round(rawM.size/5*0.95),round(rawM.size/5*0.96)) 
                 pan,tilt = GetPanTilt(random_index,rawM)  
                 MotorControl(pan,tilt)      
                 ServoReady = 0
             elif cnt == 1 :
-#                 x = file[cnt * 3]
-#                 y = file[cnt * 3 + 1]
-#                 z = file[cnt * 3 + 2]
-                #pan, tilt = cart2sph(x, y, z)
                 random_index = random.randrange(round(rawM.size/5*0.95),round(rawM.size/5*0.96))
                 pan,tilt = GetPanTilt(random_index,rawM) 
                 MotorControl(pan,tilt) 
                 ServoReady = 0
             elif cnt == 2 :
-                # Add x, y, z
-#                 x = file[cnt * 3]
-#                 y = file[cnt * 3 + 1]
-#                 z = file[cnt * 3 + 2]
-                #pan, tilt = cart2sph(x, y, z)
                 random_index = random.randrange(round(rawM.size/5*0.95),round(rawM.size/5*0.96))
                 pan,tilt = GetPanTilt(random_index,rawM) 
                 MotorControl(pan,tilt) 
                 ServoReady = 0
             elif cnt == 3 :
-                # Add x, y, z
-#                 x = file[cnt * 3]
-#                 y = file[cnt * 3 + 1]
-#                 z = file[cnt * 3 + 2]
-#                 pan, tilt = cart2sph(x, y, z)
                 random_index = random.randrange(round(rawM.size/5*0.95),round(rawM.size/5*0.96))
                 pan,tilt = GetPanTilt(random_index,rawM) 
                 MotorControl(pan,tilt) 
                 ServoReady = 0
             elif cnt == 4 :
-                # Add x, y, z
-#                 x = file[cnt * 3]
-#                 y = file[cnt * 3 + 1]
-#                 z = file[cnt * 3 + 2]                
-#                 pan, tilt = cart2sph(x, y, z)
                 random_index = random.randrange(round(rawM.size/5*0.95),round(rawM.size/5*0.96))
                 pan,tilt = GetPanTilt(random_index,rawM) 
                 MotorControl(pan,tilt) 
                 ServoReady = 0
             elif cnt == 5 :
-                # Add x, y, z 
-#                 x = file[cnt * 3]
-#                 y = file[cnt * 3 + 1]
-#                 z = file[cnt * 3 + 2]
-#                 pan, tilt = cart2sph(x, y, z)
                 random_index = random.randrange(round(rawM.size/5*0.95),round(rawM.size/5*0.96))
                 pan,tilt = GetPanTilt(random_index,rawM) 
                 MotorControl(pan,tilt) 
                 ServoReady = 0     
             elif cnt == 6 :
-            #     MotorControl(pan,tilt)
                 ServoReady = 0
-            # elif cnt == 7 :
                 print(diff)
                 # Turn off laser
                 GPIO.output(tll, GPIO.LOW)
                 questions = [
                 inquirer.List('next',
-                    message="Do you want to do beetle test?",
+                    message="Do you want to do dung beetle redirection?",
                     choices=['yes', 'no'],),
                       ]
                 answers = inquirer.prompt(questions)
@@ -364,14 +328,11 @@ if __name__ == '__main__':
       
 # beetle test             
     while mode == 4 :
-        #print("here")
-        M = np.array([-4.27812537e-04,-1.06686085e-03,2.39990615e-01,6.98027054e-04,
-                     -7.62708279e-04,2.30234512e-02,1.27127849e-03,2.91672054e-03,
-                     -9.70479398e-01,7.40217951e-06,1.67404146e-05,-5.62584993e-03])
+        
         GPIO.output(tll,GPIO.HIGH)
         direction = np.array([0,90/180*math.pi]);
-        phi = 0    
-        beetle_moving_direction = phi #The angle between the movement direction of the dung beetle and the positive X axis
+        phi = 0     #The angle between the movement direction of the dung beetle and the positive X axis
+        beetle_moving_direction = phi
         direction = direction + phi # Rotate 
         if ser.in_waiting > 0: 
             line = ser.readline().decode('utf-8').rstrip()
@@ -386,17 +347,15 @@ if __name__ == '__main__':
                 print("in position")
                     
             if ServoReady == 1:
-                #print("here")
                 if cnt < 20:
                     random_x = random.randrange(0,640)
                     random_y = random.randrange(0,480)
-                   
                     print("location on image ",random_x, random_y)
                     random_location = get_corr_point(M, random_x, random_y)
                     beetle_location_x = random_location[0]
                     beetle_location_y = random_location[1];
-                    beetle_location = [beetle_location_x,beetle_location_y];                  # Detect the beetle location
-                    print("print",beetle_location)
+                    beetle_location = [beetle_location_x,beetle_location_y]; # Detect the beetle location
+                    print("beetle_location",beetle_location)
                     new_points = ConvertXYZ(beetle_location,points);
                     angle_distance = GetAngle(new_points) #new_points 
                     [index,laser_target] = GetLaserTarget(direction,angle_distance,points)
@@ -405,49 +364,7 @@ if __name__ == '__main__':
                     start = datetime.datetime.now()
                     MotorControl(pan,tilt)
                     ServoReady = 0
-        
-
-        
-          
-#     #mode 3: Simulation
-#     # When should we enter mode 3?
-#     while mode == 3 :
-#         _, frame = cap.read()
-#         gray_image = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-#         _, threshold = cv2.threshold(gray_image, 80, 255, cv2.THRESH_BINARY)
-#         threshold=~threshold
-#         # remove noise
-#         kernel =  np.ones((5,5),np.uint8)
-#         threshold = cv2.morphologyEx(threshold, cv2.MORPH_CLOSE, kernel)
-#         cnts,_ = cv2.findContours(threshold, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-        
-#         if len(cnts) != 0:
-# #             for c in cnts:
-# #                (x, y, w, h) = cv2.boundingRect(c)
-# #                area=cv2.contourArea(c)   
-# #                if 50<area<200:
-# #                    cv2.rectangle(frame, (x, y), (x + w, y + h), (255, 0, 0), 3)
-# #                    cx = round((x + w) / 2)
-# #                    cy = round((y + h) / 2)
-# #                    print(cx,cy)
-#             # find the biggest countour (c) by the area
-#             c = max(cnts, key = cv2.contourArea)   
-#             (x, y, w, h) = cv2.boundingRect(c)
-#             cv2.rectangle(frame, (x, y), (x + w, y + h), (255, 0, 0), 3)
-#             # compute the center of the contour
-#             u = round((x + w) / 2)
-#             v = round((y + h) / 2)
-#             p3 = get_corr_point(M, 130, 130)
-#             # Algorithm to redirect dung beetle!!
-            
-            
-#             # then
-#             #MotorControl(pan,tilt)            
-                       
-            
-                      
-#         cv2.imshow("Frame", frame)
-#         cv2.imshow("threshold", threshold)
+    
             
             
             
